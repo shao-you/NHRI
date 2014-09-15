99c5394d274a1e31d03aee8290f2b23beaf23743
Shao-You Wu <s54088.cs98@g2.nctu.edu.tw>
2014-09-15 12:46:35 +0800

    Update check_disease_model.cpp
    fix some bugs

--- check_disease_model.cpp
+++ check_disease_model.cpp
@@ -1,10 +1,10 @@
 #include "control.h"
 
-string parse_ped(int total_people, ifstream& ped_file, long int offset, char minor_allele, char major_allele)
-{
+string parse_ped(int total_people, ifstream& ped_file, int marker_count, char minor_allele, char major_allele)
+{//minor_allele,major_allele不為'0'
 	//check recessive & dominant models
-	int missing_tolerace_people = (1-Missing_tolerance_threshold)*total_people;
-	int model_tolerace_people = (1-Disease_model_threshold)*total_people;
+	int missing_tolerance_people = (1-Missing_tolerance_threshold)*total_people;
+	int model_tolerance_people = (1-Disease_model_threshold)*total_people;
 	
 	char pattern[CHAR_MAX_LENGTH+1]="";
 	int recessive_count = 0;
@@ -14,30 +14,32 @@ string parse_ped(int total_people, ifstream& ped_file, long int offset, char min
 	bool flag_recessive = true;
 	bool flag_dominant = true;
 	
-	int getline_round = 1;
-	if(offset >= CHAR_MAX_LENGTH) getline_round = (offset/CHAR_MAX_LENGTH)+1;
-	int remainder = offset%CHAR_MAX_LENGTH;	
-	
 	while(ped_file.eof() == false)//check a person each time
 	{
+		streampos start = ped_file.tellg();
 		ped_file.getline(pattern, CHAR_MAX_LENGTH+1);//until CHAR_MAX_LENGTH byte
 		char* tmp = strtok(pattern, " ");
 	if(!tmp) break;	
 		for(int i=0;i<5;i++) tmp = strtok(NULL, " ");
 		int affection = atoi(tmp);
+		streamoff offset = ((tmp+strlen(tmp)+1) - pattern) + marker_count*4;
+		ped_file.clear();
+		ped_file.seekg(start);
+		ped_file.seekg(offset, ped_file.cur);
 		
-		for(int i=1;i<getline_round;i++)//getline if needed
-		{
-			ped_file.clear();	
-			ped_file.getline(pattern, CHAR_MAX_LENGTH+1);//until CHAR_MAX_LENGTH byte	
-		}
-		char first = pattern[remainder];
-		char second = pattern[remainder+2];
+		char first = ped_file.get();
+		ped_file.get();//' '
+		char second = ped_file.get();
 		
+		do{
+			ped_file.clear();
+			ped_file.getline(pattern, CHAR_MAX_LENGTH+1);
+		}while((ped_file.rdstate() & std::ifstream::failbit ) != 0);//把剩下getline完(至少還剩' ')
+
 		if(first=='0' || second=='0')//allele missing 
 		{
 			missing_count++;
-			if(missing_count > missing_tolerace_people) return " m";
+			if(missing_count > missing_tolerance_people) return " m";
 			continue;
 		}
 			/*
@@ -51,14 +53,14 @@ string parse_ped(int total_people, ifstream& ped_file, long int offset, char min
 			if(flag_recessive && !(first==minor_allele && second==minor_allele)) 
 			{
 				recessive_count++;
-				if(recessive_count > model_tolerace_people) flag_recessive = false;//不再檢查recessive
+				if(recessive_count > model_tolerance_people) flag_recessive = false;//不再檢查recessive
 			}
 			//
 			if( flag_dominant && 
 				!((first==minor_allele && second==minor_allele) || (first==minor_allele && second==major_allele)) ) 
 			{
 				dominant_count++;
-				if(dominant_count > model_tolerace_people) flag_dominant = false;//不再檢查dominant
+				if(dominant_count > model_tolerance_people) flag_dominant = false;//不再檢查dominant
 			}
 		}
 		else if(affection == 1)
@@ -67,19 +69,20 @@ string parse_ped(int total_people, ifstream& ped_file, long int offset, char min
 				!((first==minor_allele && second==major_allele) || (first==major_allele && second==major_allele)) ) 
 			{
 				recessive_count++;
-				if(recessive_count > model_tolerace_people) flag_recessive = false;//不再檢查recessive
+				if(recessive_count > model_tolerance_people) flag_recessive = false;//不再檢查recessive
 			}
 			//
 			if(flag_dominant && !(first==major_allele && second==major_allele)) 
 			{
 				dominant_count++;
-				if(dominant_count > model_tolerace_people) flag_dominant = false;//不再檢查dominant
+				if(dominant_count > model_tolerance_people) flag_dominant = false;//不再檢查dominant
 			}
 		}
 		else//affection missing 
 		{
 			missing_count++;
-			if(missing_count > missing_tolerace_people) return " m";
+			if(missing_count > missing_tolerance_people) return " m";
+			continue;
 		}
 		if(flag_recessive==false && flag_dominant==false) return " n";
 	}
@@ -103,7 +106,6 @@ void check_disease_model(int total_people, int chr)
 	
 	streamoff position_start;
 	position_start = ped_file.tellg();
-	long int offset = 12;//first 6 columns
 	int marker_count = -1;
 
 	while(fre_result.eof() == false)//check a marker each time
@@ -141,9 +143,8 @@ void check_disease_model(int total_people, int chr)
 		else if(first_rate == second_rate) {valid_marker<<marker<<" u"<<endl;continue;}//two alleles have the same rate
 		
 		if(first_allele == '0' || second_allele == '0') {valid_marker<<marker<<" u"<<endl;continue;}//missing allele
-		
-		offset += marker_count*4;
-		string result = parse_ped(total_people,ped_file,offset,minor_allele,major_allele);
+
+		string result = parse_ped(total_people,ped_file,marker_count,minor_allele,major_allele);
 		valid_marker<<marker<<result<<endl;
 		
 		ped_file.clear();
@@ -154,7 +155,7 @@ void check_disease_model(int total_people, int chr)
 	ped_file.close();
 	valid_marker.close();
 }
-/*int main()
+int main()
 {
-	check_disease_model(1378,3);
-}*/
+	check_disease_model(1355,3);
+}/**/
